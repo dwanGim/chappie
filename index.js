a84b04eb24abfb598139d17257fa9306fbae5d5c
dwanGim <nugoory20@gmail.com>
2023-09-06 20:17:19 +0900

    chappie 1.21

--- index.js
+++ index.js
@@ -25,6 +25,19 @@ document.addEventListener('DOMContentLoaded', function () {
     // console.log("TOTAL_PAGES"+TOTAL_PAGE);
     // console.log(tabBtns)
 
+    // fetch('header.html')
+    //     .then(response => response.text())
+    //     .then(data => {
+    //     document.getElementById('header').innerHTML = data;
+    // });
+
+
+    // fetch('footer.html')
+    //     .then(response => response.text())
+    //     .then(data => {
+    //     document.getElementById('footer').innerHTML = data;
+    // });
+
     goAheadCat(CURRENT_PAGE); // 고양아 말해
     
     // moveToPage(1);
